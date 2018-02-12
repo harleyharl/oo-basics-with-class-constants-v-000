846208337508608da1ea8bb87e50bb335e27ac80
Harley Rayner <harleymteden@gmail.com>
2018-02-12 23:40:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,7 +8,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if BRANDS.include? @brand
+    end
+    else
+      BRANDS << brand
+    end
   end
 
   # binding.pry
